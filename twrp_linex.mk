afeef341d57cbfdcb4b0855ffaed90046493c19d
ThRE Team <threteam@gmail.com>
2026-05-17 07:52:39 +0700

    update mk

--- twrp_linex.mk
+++ twrp_linex.mk
@@ -3,15 +3,15 @@
 # ==========================================
 
 # Devices
-PRODUCT_DEVICE := "linex"
-PRODUCT_NAME := "twrp_linex"
-PRODUCT_BRAND := "Xiaomi"
+PRODUCT_DEVICE := linex
+PRODUCT_NAME := twrp_linex
+PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
-PRODUCT_MODEL := "Generic"
+PRODUCT_MODEL := Generic
 
-# FOX_VERSION := "R12.0"
-FOX_BUILD_TYPE := "Generic"
-FOX_VARIANT := "Enjoy"
+# FOX_VERSION := R12.0
+FOX_BUILD_TYPE := Generic
+FOX_VARIANT := Enjoy
 FOX_VANILLA_THEME := 1
 FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER := 1
 FOX_DISABLE_BOOTLOOP_DETECTOR := 1
@@ -20,7 +20,6 @@ FOX_DISABLE_BOOTLOOP_DETECTOR := 1
 FOX_REPLACE_BUSYBOX := 1
 FOX_USE_BASH_SHELL := 1
 FOX_USE_NANO_EDITOR := 1
-FOX_REMOVED_MODULES := "fox_theme_manager fox_fox_modules"
 
 # Mirror ramdisk
 LOCAL_ROOT_MIRROR := device/xiaomi/linex/recovery/root
